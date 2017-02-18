55f9a14a33342d754ac1a910ed757d057d3430ce
ksky521 <ksky521@gmail.com>
2017-02-18 12:21:04 +0800

    开始

--- src/mpeditor.js
+++ src/mpeditor.js
@@ -1,27 +1,54 @@
 require('./css/common.scss')
 require('./css/mpeditor.scss')
+require('./css/theme-white.scss')
 
 console.log('success!')
 import $ from 'jQuery'
 
-var ace = require('brace')
-require('brace/mode/markdown')
-require('brace/theme/dawn')
-console.log(ace.edit)
-console.log($('body').css('background-color'))
+// ace
+import * as ace from 'brace'
+import 'brace/mode/markdown'
+import 'brace/theme/dawn'
 
-var showdown = require('showdown')
-require('./js/showdown-plugins/showdown-prettify-for-wechat.js')
-require('./js/showdown-plugins/showdown-github-task-list.js')
-require('./js/showdown-plugins/showdown-footnote.js')
-
-require('./js/google-code-prettify/run_prettify.js')
-
-var converter = new showdown.Converter({
+// showdown
+import showdown from 'showdown'
+import './js/showdown-plugins/showdown-prettify-for-wechat.js'
+import './js/showdown-plugins/showdown-github-task-list.js'
+import './js/showdown-plugins/showdown-footnote.js'
+const converter = new showdown.Converter({
   extensions: ['prettify', 'tasklist', 'footnote'],
   tables: true
 })
 
+// 语法高亮
+import './js/google-code-prettify/run_prettify.js'
+
+$(() => {
+  const editor = initEditor('js-mpe-textarea')
+  const $textarea = $('#js-mpetextarea')
+  const $win = $(window)
+    // const $perview = $('#js-mpe-perview')
+  const resizeHandler = () => {
+    let height = $win.height()
+    $textarea.height(height)
+  }
+
+  $win.resize(resizeHandler).resize()
+})
+
+// 初始化编辑器
+function initEditor(id) {
+  let editor = ace.edit(id)
+  editor.setOption('spellcheck', true)
+  editor.setOption('scrollPastEnd', true)
+  editor.renderer.setShowGutter(false)
+  editor.renderer.setPrintMarginColumn(false)
+  editor.getSession().setUseWrapMode(true)
+  editor.getSession().setNewLineMode('unix')
+  editor.getSession().setMode('ace/mode/markdown')
+  editor.setTheme('ace/theme/dawn')
+  return editor
+}
 var PR = require('PR')
 var md = 'I am using __markdown__.\n' +
   '```js\n' +
