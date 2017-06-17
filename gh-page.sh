89ded1e0a007fd8c0259ed951ca14143ac5cec6c
ksky521 <ksky521@gmail.com>
2017-06-17 11:21:07 +0800

    release

--- gh-page.sh
+++ gh-page.sh
@@ -12,6 +12,5 @@ git init
 git add -A
 date_str=`date "+DATE: %m/%d/%Y%nTIME: %H:%M:%S"`
 git commit -m "build on $date_str"
-exit
 echo 'push remote github'
 git push -u git@github.com:ksky521/mpeditor.git master:gh-pages --force
