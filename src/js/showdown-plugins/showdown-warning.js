f10df69e9622820234683bb2e675ad2e15c121ec
wangyongqing01 <wangyongqing01@baidu.com>
2020-10-23 22:12:43 +0800

    增加一些特殊样式

--- src/js/showdown-plugins/showdown-warning.js
+++ src/js/showdown-plugins/showdown-warning.js
@@ -11,8 +11,14 @@ showdown.extension('warning', () => {
                     .split('\n')
                     .map(text => {
                         if (/^!>/.test(text)) {
-                            text = `<blockquote style="border-left:6px solid #F56C6C;background-color:#fef0f0"><p>${text.replace(
+                            text = `<blockquote style="color:#f56c6c;border-left-color: #f56c6c;background-color:#fef0f0"><p>${text.replace(
                                 /^!>/,
+                                ''
+                            )}</p></blockquote>`;
+                        }
+                        else if (/^\?>/.test(text)) {
+                            text = `<blockquote style="color:#67c23a;border-left-color: #67c23a;background-color:#f0f9eb"><p>${text.replace(
+                                /^\?>/,
                                 ''
                             )}</p></blockquote>`;
                         }
